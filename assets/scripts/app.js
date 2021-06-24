1f06b4682eb8f93daf0f15852972687e29fe3981
Gabriel Ursu <rgabrielursu@gmail.com>
2021-06-24 16:31:11 +0300

    first challenge of the Bootcamp

---
+++ assets/scripts/app.js
@@ -0,0 +1,31 @@
+const Portfolio = function() {
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"love creating beautiful things.", "want to share my knowledge with the world", "solve problems.", "am a Front-End Web Developer."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+Portfolio.typeAnimation();
